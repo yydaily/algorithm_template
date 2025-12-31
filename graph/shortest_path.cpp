cd43f2cc7ff4318ba09e9f827aac6223f3414922
xuzinan.tree <xuzinan.tree@bytedance.com>
2025-12-31 15:24:09 +0800

    新增了单源最短路的模板

---
+++ graph/shortest_path.cpp
@@ -0,0 +1,74 @@
+#include<vector>
+#include<set>
+#include<algorithm>
+namespace ShortestPath {
+    /*
+     * 单源最短路
+     * 限制：
+     *      - 复杂度O（N+M）
+     *      - 无负权
+     *      - 最长路径不超过1e18
+     *      - 有向图 / 无向图都可以
+     */
+    class Graph {
+    private:
+        int N, M; // n point, m edge
+        bool start_from_one; // 点从0开始
+        std::vector<std::vector<std::pair<long long, int>>> sons; // first = value，second = to
+        std::vector<long long> distance; // 记录每个点的最近距离
+        std::set<std::pair<long long, int>> q; // 一个临时的queue
+        std::vector<int> per; // 记录从s开始到当前位置的最短路径中，当前节点的前一个节点是什么
+        
+    public:
+        Graph(int n, int m, bool start_from_one = false) : N(n), M(m), start_from_one(start_from_one) {
+            sons = std::vector<std::vector<std::pair<long long, int>>>(N+start_from_one);
+        }
+        
+        void add_edge(int s, int t, long long v) {
+            sons[s].push_back({v, t});
+        }
+
+        /*
+         * 单源最短路，复杂度O(N+M)
+         * 会从s开始建图，构建每个从s可达的点的距离 & 路径
+         */
+        void build(int s) {
+            per = std::vector<int>(N+start_from_one, -1);
+            distance = std::vector<long long>(N+start_from_one, (long long) 1e18);
+            q.clear();
+
+            distance[s] = 0;
+            q.insert({0, s});
+            while(!q.empty()) {
+                auto [v, p] = *q.begin();
+                q.erase(q.begin());
+                if(distance[p] < v) continue;
+
+                for(auto [v2, p2] : sons[p]) {
+                    if(distance[p2] > distance[p] + v2) {
+                        q.erase({distance[p2], p2});
+                        per[p2] = p;
+                        distance[p2] = distance[p] + v2;
+                        q.insert({distance[p2], p2});
+                    }
+                }
+            }
+        }
+
+        /*
+         * build完成后才可以调用，并且二者s需要相同，纯找路径
+         * 复杂度O(path_length)
+         */
+        std::pair<long long, std::vector<int>> shortest_path(int s, int t) {
+            std::pair<long long, std::vector<int>> ret;
+            ret.first = distance[t];
+            if(ret.first == 1e18) return ret; // 不存在最短路
+            ret.second.push_back(t);
+            while(ret.second[ret.second.size()-1] != s) {
+                ret.second.push_back(per[ret.second[ret.second.size()-1]]);
+            }
+            std::reverse(ret.second.begin(), ret.second.end());
+            return ret;
+        }
+    };
+}
---
+++ graph/shortest_path.nocomment.cpp
@@ -0,0 +1,58 @@
+#include<vector>
+#include<set>
+#include<algorithm>
+namespace ShortestPath {
+    class Graph {
+    private:
+        int N, M;
+        bool start_from_one;
+        std::vector<std::vector<std::pair<long long, int>>> sons;
+        std::vector<long long> distance;
+        std::set<std::pair<long long, int>> q;
+        std::vector<int> per;
+        
+    public:
+        Graph(int n, int m, bool start_from_one = false) : N(n), M(m), start_from_one(start_from_one) {
+            sons = std::vector<std::vector<std::pair<long long, int>>>(N+start_from_one);
+        }
+        
+        void add_edge(int s, int t, long long v) {
+            sons[s].push_back({v, t});
+        }
+
+        void build(int s) {
+            per = std::vector<int>(N+start_from_one, -1);
+            distance = std::vector<long long>(N+start_from_one, (long long) 1e18);
+            q.clear();
+
+            distance[s] = 0;
+            q.insert({0, s});
+            while(!q.empty()) {
+                auto [v, p] = *q.begin();
+                q.erase(q.begin());
+                if(distance[p] < v) continue;
+
+                for(auto [v2, p2] : sons[p]) {
+                    if(distance[p2] > distance[p] + v2) {
+                        q.erase({distance[p2], p2});
+                        per[p2] = p;
+                        distance[p2] = distance[p] + v2;
+                        q.insert({distance[p2], p2});
+                    }
+                }
+            }
+        }
+
+        std::pair<long long, std::vector<int>> shortest_path(int s, int t) {
+            std::pair<long long, std::vector<int>> ret;
+            ret.first = distance[t];
+            if(ret.first == 1e18) return ret;
+            ret.second.push_back(t);
+            while(ret.second[ret.second.size()-1] != s) {
+                ret.second.push_back(per[ret.second[ret.second.size()-1]]);
+            }
+            std::reverse(ret.second.begin(), ret.second.end());
+            return ret;
+        }
+    };
+}
