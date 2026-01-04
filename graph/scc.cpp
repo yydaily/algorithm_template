89b74d253095c7ac6b7d4a62d1f7f47e95897473
xuzinan.tree <xuzinan.tree@bytedance.com>
2026-01-04 20:50:16 +0800

    增加了强连通分量的模板

---
+++ graph/scc.cpp
@@ -0,0 +1,54 @@
+#include<cassert>
+#include<vector>
+namespace SCC {
+    class Graph{
+    private:
+        int N;
+        bool start_from_one;
+        std::vector<std::vector<int>> g; // 有向图
+        std::vector<std::vector<int>> g2; // 反向图
+        std::vector<int> s; // 第一次dfs的时候记录dfs序，然后反向dfs
+        std::vector<bool> vis;
+        
+        void dfs(int u) {
+            vis[u] = true;
+            for(auto v : g[u]) {
+                if(!vis[v]) dfs(v);
+            }
+            s.push_back(u);
+        }
+        void dfs2(int a, int c) {
+            vis[a] = true;
+            sccs[c].push_back(a);
+            for(auto v : g2[a]) {
+                if(!vis[v]) dfs2(v, c);
+            }
+        }
+    public:
+        std::vector<std::vector<int>> sccs; // 保存了有哪些强连通分量，每个list是一个强连通分量
+
+        Graph(int n, int start_from_one = false) : N(n), start_from_one(start_from_one) {
+            g.resize(n+start_from_one);
+            g2.resize(n+start_from_one);
+            vis = std::vector<bool>(n+start_from_one, false);
+            s = std::vector<int>(0);
+            sccs = std::vector<std::vector<int>>(0);
+        }
+        void add_edge(int s, int t) {
+            g[s].push_back(t);
+            g2[t].push_back(s);
+        }
+        void build() {
+            for(int i = start_from_one;i<N+start_from_one;i++) {
+                if(!vis[i]) dfs(i);
+            }
+            vis = std::vector<bool>(N+start_from_one, false);
+            for(int i = s.size()-1;i>=0;i--) {
+                if(!vis[s[i]]) {
+                    sccs.push_back(std::vector<int>());
+                    dfs2(s[i], sccs.size()-1);
+                }
+            }
+        }
+    };
+}
